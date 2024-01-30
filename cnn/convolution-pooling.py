53964d0a07cc9f4eda81be91dd5b85731aa8363f
Gabriel Cziprusz <gabe.cziprusz@nashvillesoftwareschool.com>
2024-01-30 07:05:50 -0600

    new words

---
+++ cnn/convolution-pooling.py
@@ -0,0 +1,97 @@
+# importing some Python libraries and the ascent picture:
+import cv2
+import numpy as np
+from scipy import misc
+i = misc.ascent()
+
+# use the Pyplot library matplotlib to draw the image so 
+# that you know what it looks like
+import matplotlib.pyplot as plt
+plt.grid(False)
+plt.gray()
+plt.axis('off')
+plt.imshow(i)
+plt.show()
+
+#  image is stored as a NumPy array, so we can create the 
+# transformed image by just copying that array. 
+i_transformed = np.copy(i)
+size_x = i_transformed.shape[0]
+size_y = i_transformed.shape[1]
+
+# This filter detects edges nicely
+# It creates a filter that only passes through sharp edges and straight lines. 
+# Experiment with different values for fun effects.
+
+# 0, 1, 0
+# 1, -4, 1
+# 0, 1, 0
+
+# filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]] 
+# A couple more filters to try for fun!
+
+# -1, -2, -1
+#  0, 0, 0
+#  1, 2, 1
+
+# filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+
+# -1, 0, 1
+# -2, 0, 2
+# -1, 0, 1
+
+filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+ # If all the digits in the filter don't add up to 0 or 1, you 
+# should probably do a weight to get it to do so
+# so, for example, if your weights are 1,1,1 1,2,1 1,1,1
+# They add up to 10, so you would set a weight of .1 if you want to normalize them
+weight  = 1
+
+# calculate the output pixels. 
+#  Iterate over the image, leaving a 1-pixel margin, and multiply each of 
+# the neighbors of the current pixel by the value defined in the filter.
+for x in range(1,size_x-1):
+  for y in range(1,size_y-1):
+      output_pixel = 0.0
+      output_pixel = output_pixel + (i[x - 1, y-1] * filter[0][0])
+      output_pixel = output_pixel + (i[x, y-1] * filter[0][1])
+      output_pixel = output_pixel + (i[x + 1, y-1] * filter[0][2])
+      output_pixel = output_pixel + (i[x-1, y] * filter[1][0])
+      output_pixel = output_pixel + (i[x, y] * filter[1][1])
+      output_pixel = output_pixel + (i[x+1, y] * filter[1][2])
+      output_pixel = output_pixel + (i[x-1, y+1] * filter[2][0])
+      output_pixel = output_pixel + (i[x, y+1] * filter[2][1])
+      output_pixel = output_pixel + (i[x+1, y+1] * filter[2][2])
+      output_pixel = output_pixel * weight
+      if(output_pixel<0):
+        output_pixel=0
+      if(output_pixel>255):
+        output_pixel=255
+      i_transformed[x, y] = output_pixel
+
+# Plot the image. Note the size of the axes -- they are 512 by 512
+plt.gray()
+plt.grid(False)
+plt.imshow(i_transformed)
+#plt.axis('off')
+plt.show()
+
+new_x = int(size_x/2)
+new_y = int(size_y/2)
+newImage = np.zeros((new_x, new_y))
+for x in range(0, size_x, 2):
+  for y in range(0, size_y, 2):
+    pixels = []
+    pixels.append(i_transformed[x, y])
+    pixels.append(i_transformed[x+1, y])
+    pixels.append(i_transformed[x, y+1])
+    pixels.append(i_transformed[x+1, y+1])
+    pixels.sort(reverse=True)
+    newImage[int(x/2),int(y/2)] = pixels[0]
+    
+# Plot the image. Note the size of the axes -- now 256 pixels instead of 512
+plt.gray()
+plt.grid(False)
+plt.imshow(newImage)
+#plt.axis('off')
+plt.show()
--- dict.md
+++ dict.md
@@ -5,36 +5,91 @@
 # A
 # B
 # C
+- convolution
+  - convolution is a mathematical way of combining two signals to form a third signal.
+- CNN
+  - TBD
+- Clustering
+  - TBD
+- cv2 
+  - cv2 is a powerful library for working with images in Python.
 # D
+- data leakage
+  - TBD
+- Decision Trees
+  - TBD
+- dense layer
+  - TBD
+- Deep Neural Networks
+  - TBD
 # E
 - epoch
   - An epoch is when all the training data is used at once and is defined as the total number of iterations of all the training data in one cycle for training the machine learning model. 
 # F
+- filter 
+  - A filter, or kernel, in a CNN is a small matrix of weights that slides over the input data (such as an image), performs element-wise multiplication with the part of the input it is currently on.
 # G
+- Gradient Descent
+  - TBD
 # H
+- H20
+  - TBD
 # I
 # J
-- Jupyter
+- jupyter
   - Jupyter is an open-source project that provides a web-based interactive computing platform. It supports various programming languages, but it's most commonly used with languages like Python, R, and Julia.
   - The name "Jupyter" is derived from the three core programming languages it initially supported: Julia, Python, and R.
 # K
 - keras
   - Keras is a high-level neural network library that runs on top of TensorFlow.
+- kernel
+  - A filter is a collection of kernels, although the ml domain may use the two terms filter and kernel interchangeably.
 # L
+- Linear Regression 
+  - TBD
+- Logistic Regression
+  - TBD
 # M
+  - Multiple Regression 
+    - TBD
 # N
 - NumPy
   - NumPy is a Python library used for working with arrays. It also has functions for working in domain of linear algebra, fourier transform, and matrices.
 # O
+- overfitting
+  - TBD
 # P
+- pandas
+  - TBD
+- Principal Component Analysis
+  - TBD
+- pooling
+  - TBD
+- Plotly
+  - TBD
 # Q
 # R
+- Recommendation Systems
+  - TBD 
+- relu
+  - TBD 
+
 # S
+- Scikit Learn
+  - TBD
+- Seaborn
+  - TBD 
+- Support Vector Machines
+  - TBD 
 - stochastic gradient descent (sgd)
   - SGD is an iterative optimization algorithm that aims to minimize a cost function by updating the model parameters in the opposite direction of the gradient of the cost function. 
+- Sparse Categorical Crossentropy (SCC)
+  - TBD
 - Spyder
   - A free and open source scientific environment written in Python.
 # T
+- tableau
+  - TBD
 - Tensor
   - A tensor is a generalization of vectors and matrices and is easily understood as a multidimensional array. A vector is a one dimensional or first order tensor and a matrix is a two dimensional or second order tensor.
 - tensorflow
